f9514c10cc64ad3a59c4deceee98a8ccca328d0c
anupam005004 <qw6579@L76ATL0U247BJXS.cead.prd>
2015-07-21 15:11:46 -0400

    get all prime factors of numbers

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace math.primefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file location");
+            string path = Console.ReadLine();
+            try
+            {
+                FileInfo file = new FileInfo(path);
+                List<string> lstNumbers = new List<string>();
+                using (StreamReader reader = file.OpenText())
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        lstNumbers.Add(reader.ReadLine());
+                    }
+                }
+                lstNumbers.ForEach(n =>
+                {
+                    BigInteger num;
+                    List<BigInteger> factors;
+                    string outputString = "";
+                    if (BigInteger.TryParse(n, out num))
+                    {
+                        math math = new math();
+                        factors = math.GetPrimeFactors(num);
+                        factors.ForEach(f => outputString += f.ToString() + ", ");
+                        Console.WriteLine("Prime factors of {0} is/are {1}  ", num, outputString.Remove(outputString.LastIndexOf(",")));
+                    }
+                    else
+                        Console.WriteLine("Invalid # " + n);
+
+                });
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+      
+    }
+}
---
+++ Utilities/math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math.primefactor
+{
+    public class math
+    {
+      static  public List<BigInteger> GetPrimeFactors(BigInteger inputNumber)
+        {
+            List<BigInteger> factors = new List<BigInteger>();
+            int nextFactor = 2;
+            while (inputNumber > 1)
+            {
+                if (inputNumber % nextFactor > 0)
+                {
+                    nextFactor += (nextFactor == 2) ? 1 : 2;
+                    do
+                    {
+                        if (inputNumber % nextFactor == 0)
+                        {
+                            break;
+                        }
+
+                        nextFactor += 2;
+                    } while (nextFactor < inputNumber);
+                }
+                inputNumber /= nextFactor;
+                factors.Add(nextFactor);
+            }
+            return factors;
+        }
+    }
+}
